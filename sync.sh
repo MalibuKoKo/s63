f736af5bf038c3e790e09519848e700fa8f874a5
Nicolas COLLET <nicolas.collet@aldebaran.com>
2025-02-23 03:22:08 +0100

    chore: add dependancies

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-HOST="pi@192.168.0.25"
+HOST="dietpi"
 #HOST="pi@172.24.1.1"
 
-rsync -av --delete --exclude=node_modules /Users/juju/Documents/projects/s63/ $HOST:/home/pi/s63
-ssh $HOST -t 'sudo pm2 restart all'
+rsync -av --delete --exclude=.devbox --exclude=devbox.lock --exclude=.git --exclude=node_modules /home/nicolas.collet/workspace/github.com/MalibuKoKo/s63/ $HOST:/home/dietpi/s63
+# ssh $HOST -t 'sudo pm2 restart all'
