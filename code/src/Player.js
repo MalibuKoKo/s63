507f70113d135b290a50c075264ff0bacba0360e
Nicolas COLLET <nicolas.collet@aldebaran.com>
2025-03-16 11:02:04 +0100

    build: update

--- code/src/Player.js
+++ code/src/Player.js
@@ -5,32 +5,42 @@ class Player {
     this.playing = null;
     this.sine = this.sine.bind(this);
   }
+
   stop() {
     console.log("Player", "stop");
     if (this.playing) {
-      this.playing.removeListener("finish", this.sine);
-      this.playing.end();
+      this.playing.removeListener("close", this.sine);
+      this.playing.kill(); // Utilisation de kill() au lieu de end()
       this.playing = null;
     }
   }
+
   sine() {
     console.log("Player", "sine");
     this.stop();
     this.playing = sine();
   }
-  play(stream, options={sine: true}) {
+
+  play(stream, options = { sine: true }) {
     this.stop();
-    const speaker = play(stream);
-    this.playing = speaker;
+    const process = play(stream); // Renvoie un processus spawn
+
+    this.playing = process;
+
     return new Promise((resolve, reject) => {
-      speaker.on("finish", () => {
+      process.on("close", () => { // Remplace "finish" par "close"
         resolve();
         if (options.cb) {
-          options.cb(); // Correction : options.cb() au lieu de cb()
+          options.cb();
         }
         if (options.sine) {
           this.sine();
         }
+      });
+
+      process.on("error", (err) => {
+        console.error("Player error:", err);
+        reject(err);
       });
     });
   }
